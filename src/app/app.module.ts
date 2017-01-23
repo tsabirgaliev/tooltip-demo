59873c80db6eb910d94a7d7135a63593832a072f
Tair Sabirgaliev <tair.sabirgaliev@gmail.com>
2017-01-23 17:04:32 +0500

    tooltip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { TooltipModule } from 'ng2-bootstrap/tooltip';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    TooltipModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
